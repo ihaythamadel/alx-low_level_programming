4cbeaf03b66895d592d02d0121a3cb22bed60f8e
ihaythamadel <ihaythamadel@gmail.com>
2023-02-16 13:55:37 -0800

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
-/**                                                  
+/**
 * main - prints a string and standard error
- * Return: 1                                         
+ * Return: 1
 */
 int main(void)                                       {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
